3c5cfdc55e87361ff49e60910ca50dccdd00e380
Lee Lup Yuen <luppy@appkaki.com>
2022-07-01 16:29:47 +0800

    Clean up

--- lvgltest.zig
+++ lvgltest.zig
@@ -47,30 +47,30 @@ pub export fn lvgltest_main(
     debug("Zig LVGL Test", .{});
     _ = _argc;
     _ = _argv;
-    var disp_drv: ?*c.lv_disp_drv_t = c.get_disp_drv();
-    var disp_buf: ?*c.lv_disp_buf_t = c.get_disp_buf();
+    const disp_drv = c.get_disp_drv();
+    const disp_buf = c.get_disp_buf();
     c.lv_init();
     c.init_disp_buf(disp_buf);
     c.init_disp_drv(disp_drv, disp_buf, monitor_cb);
-    if (c.lcddev_init(disp_drv) != @as(c_int, 0)) {
-        if (c.fbdev_init(disp_drv) != @as(c_int, 0)) {
+    if (c.lcddev_init(disp_drv) != 0) {
+        if (c.fbdev_init(disp_drv) != 0) {
             return 1;
         }
     }
     _ = c.lv_disp_drv_register(disp_drv);
     _ = c.tp_init();
-    var indev_drv: [*c]c.lv_indev_drv_t = c.get_indev_drv();
+    const indev_drv = c.get_indev_drv();
     c.init_indev_drv(indev_drv, c.tp_read);
     create_widgets();
     c.tp_cal_create();
     while (true) {
         _ = c.lv_task_handler();
-        _ = c.usleep(@bitCast(c.useconds_t, @as(c_int, 10000)));
+        _ = c.usleep(10000);
     }
     return 0;
 }
 
-pub fn create_widgets() callconv(.C) void {
+pub export fn create_widgets() void {
     var screen: ?*c.lv_obj_t = c.lv_scr_act();
     var label: ?*c.lv_obj_t = c.lv_label_create(screen, null);
     c.lv_label_set_long_mode(label, @bitCast(c.lv_label_long_mode_t, @truncate(i8, c.LV_LABEL_LONG_BREAK)));
@@ -81,13 +81,14 @@ pub fn create_widgets() callconv(.C) void {
     c.lv_obj_align(label, null, @bitCast(c.lv_align_t, @truncate(i8, c.LV_ALIGN_CENTER)), @bitCast(c.lv_coord_t, @truncate(c_short, @as(c_int, 0))), @bitCast(c.lv_coord_t, @truncate(c_short, -@as(c_int, 30))));
 }
 
-pub fn monitor_cb(arg_disp_drv: ?*c.lv_disp_drv_t, arg_time_1: u32, arg_px: u32) callconv(.C) void {
-    var disp_drv = arg_disp_drv;
-    _ = disp_drv;
-    var time_1 = arg_time_1;
-    _ = time_1;
-    var px = arg_px;
-    _ = px;
+pub export fn monitor_cb(
+    _disp_drv: ?*c.lv_disp_drv_t, 
+    _time: u32, 
+    _px: u32
+) void {
+    _ = _disp_drv;
+    _ = _time;
+    _ = _px;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
